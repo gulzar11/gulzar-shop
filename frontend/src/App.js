4c0a04d9e1468cc9e10b46d8141dd54c3a7693a6
gulzar11 <muhammad11.gulzar@gmail.com>
2022-05-30 23:01:04 +0530

    video3 create react app

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">gulzar-shop</a>
+      </header>
+      <main>product lists</main>
+    </div>
+  );
+}
+
+export default App;
