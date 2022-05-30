9cccdb8fc8ba230e631a5673fd73557279acd8e8
gulzar11 <muhammad11.gulzar@gmail.com>
2022-05-31 00:03:05 +0530

    vid5 data inserted

--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 1 . introduction
 2. install tools
 3. create react app
-4. create git repo
+4. create git repo 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-
+import data from './data';
 
 function App() {
   return (
@@ -6,9 +6,31 @@ function App() {
       <header>
         <a href="/">gulzar-shop</a>
       </header>
-      <main>product lists</main>
+      <main>
+        <h1>Featured Products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  <strong>${product.price}</strong>
+                </p>
+                <button>Add to cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
 
 export default App;
+
+
